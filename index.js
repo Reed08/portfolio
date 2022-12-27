1a56533f8ddb79acff9c21cb9ddee7ddc9a81dc1
Reed08 <96321587+Reed08@users.noreply.github.com>
2022-12-26 21:01:20 -0600

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const colorSchemeSelect = document.getElementById('color-scheme-select');
-
-colorSchemeSelect.addEventListener('change', (event) => {
-  const colorScheme = event.target.value;
-  document.body.className = `color-scheme-${colorScheme}`;
-});
